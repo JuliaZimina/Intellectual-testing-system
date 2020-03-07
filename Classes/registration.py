4663283879b19911ab0145f1998040464a531255
DariaAlium <daria.kurguzikova@intel.com>
2020-03-07 12:31:47 +0300

    07.03 by DariaAlium

--- Classes/registration.py
+++ Classes/registration.py
@@ -1,118 +1,133 @@
-#from emal.massage import EmalMassage
+from emal.massage import EmalMassage
 import smtplib
 
 from dataProcessing import users
 
 EMAIL = "zeiwjfew@yandex.ru"
 PASSWORD = "ooP123"
-COUNT_OF_PASSWORD = 3
 
 
-def check_login(login):
+def checkLogin(login):
     
     if login in users:
         return False
     return True
-    
-def check_password(password):
+ 
+
+ 
+def checkPassword(password):
 
     if password < 8:
         return False
     return True
 
-def registration(login,password,name,surname,father_name,date_of_birth,group,secret_question,secret_answer,email,tel,photo=""):
-    #login = input()
-    while not check_login(login):
-        print("Such name already exists \n Please, enter another")
-        login = input()
 
-    print("Your password should be larger than 8")
-    #password = input()
-    while not check_password(password):
-        print("Try again")
-        password = input()
 
-    name = input()
-    surname = input()
-    father_name = input()
-    group = input()
-    secret_question = input()
-    answer = input()
-    main = input()
-    tel = input()
-    return True
-
-''''
-    #вынеси в отдельную функцию
+def sendMessage(mail):
     msg = EmalMassage()
     msg['Subject'] = "Подтверждение регистрации"
     msg['From'] = EMAIL
     msg['To'] = mail
-    msg.set_content("Подтвердите регистрацию")
+    msg.set_content("Вы подтвердили регистрацию")
     
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL, PASSWORD)
         smtp.send_massage(msg)
+
+
+
+def checkNames(name):
+    if name == '':
+        return False
+    return True
+
+
+
+def registration(login,password,name,surname,father_name,date_of_birth,group,secret_question,secret_answer,email,tel,photo=""):
+
+    if not checkLogin(login):
+        raise login_exception("Such name already exists")
+        
+
+    if not checkPassword(password):
+         raise password_exception("Your password should be larger than 8")
+
+
+    if not checkNames(name):
+        raise name_exception("This field is requried")
     
-    #make an object,нет,внести все данные в users
+    if not checkNames(surname):
+        raise name_exception("This field is requried")
     
-def reestablishPassword(login):
     
-    print(users[login][secret_question])
-    answer = input()
-    if answer == users[login][secret_answer]:
-        return True
-    return False
+    if (group != '18би1') & (group != '18би2'):
+         raise group_exception("There are only 2 types of group")
     
-'''''
+    
+    if secret_question is not in secret_questions:
+        raise question_exception("There are no such question")
+        
+
+    if not checkNames(answer):
+        raise name_exception("This field is requried")
+    
+
+    if not checkNames(mail):
+        raise name_exception("This field is requried")
+    
+    users[login] = {'password': password, 'status': 'regular_user', 'ban' = False, 'name' =  name, 'date_of_birth' = date_of_birth, 'tel':tel, 'father_name' = father_name,
+            'surname' = surname, 'group' = group, 'secret_question' = secret_question, 'secret_answer' = answer, 'email' = email, 'photo' = photo}
+
+    
+    sendMessage(email)
+    
+    
+    return templateUser()
+
+    
+       
+def reestablishPassword_step1(login, answer):
+    
+    if answer != users[login]['secret_answer']:
+        raise answer_exception("secret answer is wrong")
+    return True
+  
+
+  
+def reestablishPassword_step2(login, new_password):
+    
+     if new_password == users[login][password]:
+         raise password_exception("New password shouldn't be equal to old one")
+    return True
+ 
+ 
+
 def logIn(login, password): # вход
 
-    count_of_passing = 0
-    '''''
-    while count < COUNT_OF_PASSWORD:
-        login, password = input(), input()
-        
-        try:
-            if login not in user:
-                raise loginException("There are no such user")
-            
-            if user[login][password] != password:
-                countOfPassing += 1 
-                raise passwordException("Wrong password")
-            
-            else:
-                
-                break
-        
-        except:
-            pass
-        
-    print("Do you wanna reestablish password (Y/N)")
     
-    if input() == "Y":
-        if reestablishPassword(login):
-            #window for changing password
-            print("Enter new password")
-            
-            newPassword = input()
-            
-            if newPassword == user[login][password]:
-                
-                raise passwordException("New password shouldn't be equal to old one")
-                
-                #отдельное окно в которое пользователь вводит указанные сведения 
-                
-            
-            user[login][password] = newPassword
-            
-            
-    else:
-        pass
-        #close the window
+    if login not in user:
+        raise login_exception("There are no such user")
     
-    templateUser = "" #
-    return user
+    if users[login]['password'] != password:
+        raise password_exception("Wrong password")
 
-'''''
-def tmpLogIn(login): #временный вход
-    pass
+    return True
+
+
+
+def tmpLogIn(login, old_password, new_password): #временный вход
+    if login not in users:
+       raise login_exception("There are no such user")
+    
+    if users[login]['password'] != old_password:
+        raise password_exception("Wrong password")
+        
+    users[login]['password'] = password
+       
+       
+    return True
+    
+
+
+
+
