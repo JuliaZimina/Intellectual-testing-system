6674722788d03828412010669c8456dee66b3ec5
Veronika-boop <60421829+Veronika-boop@users.noreply.github.com>
2020-03-08 23:12:35 +0300

    Update administrator.py

--- Classes/administrator.py
+++ Classes/administrator.py
@@ -1,6 +1,11 @@
+import ssms_su
 from Classes.analyst import *
 
 
 class Administrator(Analyst):
     def __init__(self):
         pass
+    def block_user():
+	ban = true
+	smsapi = ssms_su.smsapi(user)
+	smsapi.push_msg("Hello, you are in block!", user)
