97217a7d3aa74eab1682b8df8cc1ae9a63ae993f
Katya914 <60344949+Katya914@users.noreply.github.com>
2020-03-09 16:13:18 +0300

    Update statistics.py

--- Classes/statistics.py
+++ Classes/statistics.py
@@ -4,5 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+#столбчатые диаграммы по граппам вопросов 
 sns.catplot(x="группа вопросов", y="количество отвечавших", data=pd.read_csv('Data/Ratings/groupStatistics.sys'), height=6, kind="bar",  hue = 'группа людей')
 sns.catplot(x="группа вопросов", y="количество правильных", data=pd.read_csv('Data/Ratings/groupStatistics.sys'), height=6, kind="bar",  hue = 'группа людей')
