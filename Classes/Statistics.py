3a2b3e6e3c75555b50d43b63da059fb4053d8e49
Katya914 <60344949+Katya914@users.noreply.github.com>
2020-03-09 16:09:45 +0300

    Add files via upload

---
+++ Classes/Statistics.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+sns.catplot(x="группа вопросов", y="количество отвечавших", data=pd.read_csv('Data/Ratings/groupStatistics.sys'), height=6, kind="bar",  hue = 'группа людей')
+sns.catplot(x="группа вопросов", y="количество правильных", data=pd.read_csv('Data/Ratings/groupStatistics.sys'), height=6, kind="bar",  hue = 'группа людей')
