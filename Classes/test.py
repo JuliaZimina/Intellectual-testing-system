000da727c8192bde4ed08f7a50c998cb7f1b7073
el1zager <60437857+el1zager@users.noreply.github.com>
2020-03-09 21:40:58 +0300

    new

--- Classes/test.py
+++ Classes/test.py
@@ -1,14 +1,36 @@
+from dataProcessing import *
+
+def printQuestion(file):
+    lines = [line.strip() for line in file]
+    for i in range(5):
+        print(lines[i])
+
 class Test:
-    def __init__(self,theme):
-        self.theme=theme
-        self.end_of_test=False
+    """Test model"""
+
+    def __init__(self, field):
+        self.field = field
+        self.end_of_test = False
         pass
+
 #генерирует следующий вопрос
     def getNextQuestion(self):
+
         pass
+
 #пользователь отправляет свой ответ
     def sendAnswer(self, answer):
         pass
 
     def getResult(self):
         pass
+
+    def launch(self):
+        print("Вы выбрали тест по теме: " + self.field)
+        if (self.field == "История"):
+            f1 = open('tests.sys', 'r')
+            printQuestion(f1)
+            a = input()
+
+test = Test("История России")
+test.launch()
