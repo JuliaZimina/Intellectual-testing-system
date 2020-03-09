378b1a2345a4541ea6ccaafe8228d2c5d8c5696c
Veronika-boop <60421829+Veronika-boop@users.noreply.github.com>
2020-03-09 12:20:24 +0300

    Update administrator.py

--- Classes/administrator.py
+++ Classes/administrator.py
@@ -9,3 +9,21 @@ class Administrator(Analyst):
 	ban = true
 	smsapi = ssms_su.smsapi(user)
 	smsapi.push_msg("Hello, you are in block!", user)
+    def delete_question():
+	del dictionary['question']
+    def close_group():
+	del dictionary['group'] 
+	'''dictionary.popitem()'''
+    def watch_questions(dectionary):
+	print(dictinary)
+    def add_question(question):
+	dictinary['group'] = question
+	time =  time
+	dictionary['varianti otveta'] = varianti
+	return dictinary
+    def edit_question(question):
+	dictionary['question'] = question
+	return dictionary
+    def roles():
+	user = analyst
+	user = user
