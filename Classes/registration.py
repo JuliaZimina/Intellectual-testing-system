af91eb168d8ac8200fb1e62f703b333fe0f7add2
dariaAlium <54068485+dariaAlium@users.noreply.github.com>
2020-03-10 19:30:07 +0300

    Update registration.py

--- Classes/registration.py
+++ Classes/registration.py
@@ -1,3 +1,6 @@
+import urllib
+import json
+import time
 #from emal.massage import EmalMassage
 import smtplib
 from Classes.User import *
@@ -6,6 +9,36 @@ from dataProcessing import users,recovery_requests
 #from secretQuestions import secret_questions
 EMAIL = "zeiwjfew@yandex.ru"
 PASSWORD = "ooP123"
+
+def send_sms(phones, text, total_price=0):
+    login = 'userlog'       # Логин в smsc
+    password = 'myPas1'     # Пароль в smsc
+    sender = 'Python'    # Имя отправителя
+    # Возможные ошибки
+    errors = {
+        1: 'Ошибка в параметрах.',
+        2: 'Неверный логин или пароль.',
+        3: 'Недостаточно средств на счете Клиента.',
+        4: 'IP-адрес временно заблокирован из-за частых ошибок в запросах. Подробнее',
+        5: 'Неверный формат даты.',
+        6: 'Сообщение запрещено (по тексту или по имени отправителя).',
+        7: 'Неверный формат номера телефона.',
+        8: 'Сообщение на указанный номер не может быть доставлено.',
+        9: 'Отправка более одного одинакового запроса на передачу SMS-сообщения либо более пяти одинаковых запросов на получение стоимости сообщения в течение минуты. '
+    }
+    # Отправка запроса
+    url = "http://smsc.ru/sys/send.php?login=%s&psw=%s&phones=%s&mes=%s&cost=%d&sender=%s&fmt=3" % (login, password, phones, text, total_price, sender)
+    answer = json.loads(urllib.urlopen(url).read())
+    if 'error_code' in answer:
+        # Возникла ошибка
+        return errors[answer['error_code']]
+    else:
+        if total_price == 1:
+            # Не отправлять, узнать только цену
+            print 'Будут отправлены: %d SMS, цена рассылки: %s' % (answer['cnt'], answer['cost'].encode('utf-8'))
+        else:
+            # СМС отправлен, ответ сервера
+            return answer
 
 def checkLogin(login):
     
