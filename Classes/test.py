3b54bd801436eba13f26a2b245ab8a7c0f280aa3
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2020-03-10 10:12:58 +0300

    Изменения в тесте

--- Classes/test.py
+++ Classes/test.py
@@ -3,7 +3,7 @@ from dataProcessing import *
 class Test:
     """Test model"""
 
-    def __init__(self, field, sum_right):
+    def __init__(self, field, sum_right=0):
         self.field = field
         self.sum_right = sum_right
         self.ex_quest = []
@@ -12,26 +12,26 @@ class Test:
 
 #генерирует следующий вопрос
     def getNextQuestion(self):
-        data = read_tests("tests.sys")
-        for key in data:
+
+        for key in tests:
             if self.field != "general":
                 if key == self.field:
-                    for j in data[key]:
+                    for j in tests[key]:
                         for i in self.ex_quest:
                             if i == j:
                                 continue
-                    self.ex_quest.append(data[key][j])
-                    return data[key][j]
+                    self.ex_quest.append(tests[key][j])
+                    return tests[key][j]
             else:
-                for j in data[key]:
+                for j in tests[key]:
                     for i in self.ex_quest:
                         if i == j:
                             continue
                         else:
                             if len(self.ex_quest)%2 == 0:
                                 break
-                self.ex_quest.append(data[key][j])
-                return data[key][j]
+                self.ex_quest.append(tests[key][j])
+                return tests[key][j]
 
 #пользователь отправляет свой ответ
     def sendAnswer(self, answer, time):
@@ -39,3 +39,9 @@ class Test:
 
     def getResult(self):
         pass
+test1=Test("История")
+print(test1.getNextQuestion())
+print(test1.getNextQuestion())
+test2=Test("general")
+print(test2.getNextQuestion())
+print(test2.getNextQuestion())
