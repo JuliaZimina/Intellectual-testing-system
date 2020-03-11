e7b800492fc72f559bd9e8c292d8561cfaf6b846
JuliaZimina <60311865+JuliaZimina@users.noreply.github.com>
2020-03-11 14:01:18 +0300

    Тест

--- Classes/test.py
+++ Classes/test.py
@@ -19,6 +19,7 @@ class Test:
                         if [j,tests[key][j]] in self.ex_quest:
                             continue
                         self.ex_quest.append([j,tests[key][j]])
+                        print("ex_q", self.ex_quest)
                         return [j,tests[key][j]]
             else:
                 for j in tests[key]:
@@ -29,12 +30,16 @@ class Test:
                     self.array_themes.append(key)
                     self.array_themes.append([j,tests[key][j]])
                     self.ex_quest.append([j,tests[key][j]])
+
                     return [j,tests[key][j]]
 
 
 # пользователь отправляет свой ответ
     def sendAnswer(self, answer, time):
-        d = self.ex_quest[-1]
+        print("h")
+        d = self.ex_quest[-1][1]
+        print("hi")
+        print(d)
         if answer != d['ответ'][0]:
             self.answers.append(0)
         if (time > int(d['время']) or time <= 5 or answer != d['ответ'][0]) and self.number_of_quest < 10:
@@ -45,11 +50,11 @@ class Test:
 
     def getResult(self):
         st = ''
-        for i in range(len(self.ex_quest)-1):
+        for i in range(len(self.ex_quest)):
              if self.answers[i] == 0:
-                 st = st + "Вопрос " + str(i) + " отвечен неправильно\n"
+                 st = st + "Вопрос: " + str(i+1)+"."+self.ex_quest[i][0] + " отвечен неправильно\n"
              else:
-                 st = st + "Вопрос " + str(i) + " отвечен правильно\n"
+                 st = st + "Вопрос " + str(i+1)+"."+self.ex_quest[i][0] + " отвечен правильно\n"
         st = st + "Количество правильных ответов: " + str(self.answers.count(1)) + " из " + str(len(self.answers)) + "\n" \
              + str(int(self.answers.count(1) / len(self.answers) * 100)) + " процентов отвечено верно"
         return st
@@ -71,9 +76,10 @@ class Test:
 
 test1=Test("История")
 print(test1.getNextQuestion())
-#test1.sendAnswer("ggg",30)
+test1.sendAnswer("ggg",30)
+print(test1.getResult())
 test2=Test("general")
 print("start")
-print(test2.getNextQuestion())
-print(test2.getNextQuestion())
+#print(test2.getNextQuestion())
+#print(test2.getNextQuestion())
 
